b810a1ab9b670ea09fa5d6b03dba38169d75f1a6
Kvishnu3423 <kumar.2023.us@gmail.com>
2026-05-25 16:35:27 +0530

    Add floating Back to Top button across all pages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,6 +3,7 @@ import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import ScrollToTop from './ScrollToTop'
+import BackToTop from './BackToTop'
 
 const REVEAL_SELECTORS = [
   'section',
@@ -86,6 +87,7 @@ export default function Layout() {
       <Navbar />
       <Outlet />
       <Footer />
+      <BackToTop />
     </>
   )
 }
